69656413dbc114b212b85a35018361e6be4795dc
KimiWang <102603313+KimiWangWml@users.noreply.github.com>
2022-09-19 19:16:04 +0800

    created a mudule and imported it with turtle

--- index.py
+++ index.py
@@ -1,1 +1,15 @@
-print("hello world")
+import turtle
+import functions as func
+
+t = turtle.Pen()
+
+
+def shape(length, angle, times):
+    for i in range(times):
+        t.forward(length)
+        t.left(angle)
+
+shape(150, 133, 105)
+
+
+func.pause() 

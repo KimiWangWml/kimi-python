9be44e3cec65d6bbbc92dd605e6f51440205675e
KimiWang <102603313+KimiWangWml@users.noreply.github.com>
2022-09-26 19:17:48 +0800

    added second example

--- index.py
+++ index.py
@@ -5,4 +5,9 @@ number_of_trips = 0
 if number_of_people > number_of_seats:
     number_of_trips = number_of_people / number_of_seats
 
-print(number_of_trips)
+#print(number_of_trips)
+
+number_of_fat_people = 4
+if number_of_fat_people > 0 and number_of_people > 10:
+    number_of_seats -= number_of_fat_people
+    print(number_of_seats)

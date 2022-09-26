0560e8d5e16ca4b80589c3d940f37d157a3b3565
KimiWang <102603313+KimiWangWml@users.noreply.github.com>
2022-09-26 19:07:27 +0800

    deleted function.py

--- index.py
+++ index.py
@@ -1,1 +1,8 @@
-print("hello world")
+number_of_people = 20
+number_of_seats = 10
+number_of_trips = 0
+
+if number_of_people > number_of_seats:
+    number_of_trips = number_of_people / number_of_seats
+
+print(number_of_trips)
